0ea4f4000dfec690feaf913940f6888ffd49407b
Logan Call <logancall.dev@gmail.com>
2018-08-08 13:40:58 -0600

    start overlay

--- bundle.index.js
+++ bundle.index.js
@@ -1,2 +1,3 @@
 export { default as ContentMarquee } from './lib/js/src/ContentMarquee.bs'
 export { default as Button } from './lib/js/src/Button.bs'
+export { default as Overlay } from './lib/js/src/Overlay.bs'
